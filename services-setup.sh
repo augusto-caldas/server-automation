d6418a0ffa3de237a3c492210be196602bc83441
Luis Caldas <augusto@caldas.ie>
2025-04-27 18:23:52 +0100

    Add check if service is already installed

--- services-setup.sh
+++ services-setup.sh
@@ -1,7 +1,32 @@
 #!/usr/bin/env bash
 
-while true;
-do
+check_and_prompt() {
+  local folder=$1
+  local container=$2
+
+  if [ -d "$folder" ] || docker ps -a --format '{{.Names}}' | grep -wq "$container"; then
+    echo "Warning: Folder '$folder' or Docker container/service '$container' already exists."
+    read -rp "Do you want to remove them and continue? (y/n): " response
+
+    if [[ "$response" == "y" || "$response" == "Y" ]]; then
+      [ -d "$folder" ] && rm -rf "$folder" && echo "Removed folder '$folder'."
+
+      if docker ps -a --format '{{.Names}}' | grep -wq "$container"; then
+        docker rm -f "$container"
+        echo "Removed Docker container/service '$container'."
+      fi
+
+    else
+      echo "Installation canceled."
+      return 1
+    fi
+
+  fi
+
+  return 0
+}
+
+while true; do
   echo "Choose a service to be installed"
   echo "1. Nextcloud"
   echo "2. Vaultwarden"
@@ -21,42 +46,55 @@ do
       ;;
 
     "1")
-      echo "Nextcloud setup is not available at the moment"
+      if check_and_prompt "./nextcloud" "nextcloud"; then
+        echo "Nextcloud setup is not available at the moment."
+        # Here you would place the setup commands when available
+      fi
       ;;
 
     "2")
-      echo "Installing Vaultwarden..."
-      mkdir vaultwarden
-      wget -P ./vaultwarden "https://raw.githubusercontent.com/augusto-caldas/server-automation/refs/heads/main/setup-scripts/vaultwarden-setup.sh"
-      bash ./vaultwarden/vaultwarden-setup.sh
+      if check_and_prompt "./vaultwarden" "vaultwarden"; then
+        echo "Installing Vaultwarden..."
+        mkdir vaultwarden
+        wget -P ./vaultwarden "https://raw.githubusercontent.com/augusto-caldas/server-automation/refs/heads/main/setup-scripts/vaultwarden-setup.sh"
+        bash ./vaultwarden/vaultwarden-setup.sh
+      fi
       ;;
 
     "3")
-      echo "Installing Jellyfin..."
-      mkdir jellyfin
-      wget -P ./jellyfin "https://raw.githubusercontent.com/augusto-caldas/server-automation/refs/heads/main/setup-scripts/jellyfin-setup.sh"
-      bash ./jellyfin/jellyfin-setup.sh
+      if check_and_prompt "./jellyfin" "jellyfin"; then
+        echo "Installing Jellyfin..."
+        mkdir jellyfin
+        wget -P ./jellyfin "https://raw.githubusercontent.com/augusto-caldas/server-automation/refs/heads/main/setup-scripts/jellyfin-setup.sh"
+        bash ./jellyfin/jellyfin-setup.sh
+      fi
       ;;
 
     "4")
-      echo "Installing Minecraft server..."
-      mkdir minecraft-server
-      wget -P ./minecraft-server "https://raw.githubusercontent.com/augusto-caldas/server-automation/refs/heads/main/setup-scripts/minecraft-setup.sh"
-      bash ./minecraft-server/minecraft-setup.sh
+      if check_and_prompt "./minecraft-server" "minecraft-server"; then
+        echo "Installing Minecraft server..."
+        mkdir minecraft-server
+        wget -P ./minecraft-server "https://raw.githubusercontent.com/augusto-caldas/server-automation/refs/heads/main/setup-scripts/minecraft-setup.sh"
+        bash ./minecraft-server/minecraft-setup.sh
+      fi
       ;;
 
     "5")
-      echo "Installing MySQL..."
-      mkdir mysql
-      wget -P ./mysql "https://raw.githubusercontent.com/augusto-caldas/server-automation/refs/heads/main/setup-scripts/mysql-setup.sh"
-      bash ./mysql/mysql-setup.sh
+      if check_and_prompt "./mysql" "mysql"; then
+        echo "Installing MySQL..."
+        mkdir mysql
+        wget -P ./mysql "https://raw.githubusercontent.com/augusto-caldas/server-automation/refs/heads/main/setup-scripts/mysql-setup.sh"
+        bash ./mysql/mysql-setup.sh
+      fi
       ;;
 
     "6")
-      echo "Installing MongoDB..."
-      mkdir mongodb
-      wget -P ./mongodb "https://raw.githubusercontent.com/augusto-caldas/server-automation/refs/heads/main/setup-scripts/mongodb-setup.sh"
-      bash ./mongodb/mongodb-setup.sh
+      if check_and_prompt "./mongodb" "mongodb"; then
+        echo "Installing MongoDB..."
+        mkdir mongodb
+        wget -P ./mongodb "https://raw.githubusercontent.com/augusto-caldas/server-automation/refs/heads/main/setup-scripts/mongodb-setup.sh"
+        bash ./mongodb/mongodb-setup.sh
+      fi
       ;;
 
     *)
